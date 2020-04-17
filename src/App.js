77cb4149b575a37be698961a09033d1e63a20ebb
shafali03 <shaf03sm@gmail.com>
2020-04-17 18:12:13 +0100

    feat: Created button for increment counter

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
     return (
       <div data-test="component-app">
         <h1 data-test="counter-display">The counter is currently</h1>
+        <button data-test="increment-button">Increment counter</button>
       </div>
     );
   }
